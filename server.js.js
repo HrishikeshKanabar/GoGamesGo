d6c7ccd9a19fcd11abc8593d1f2b99bc0feb3fa1
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-11-23 20:52:44 -0500

    initial server

---
+++ server.js.js
@@ -0,0 +1,25 @@
+// Express JS dependency
+const exp = require("express");
+
+// Start
+const app = exp();
+const PORT = process.env.PORT || 3001;
+
+const games = {
+    name: 'GTA',
+    consoletype: 'Playstation',
+    releasedate: '2012',
+    type: 'war'
+  };
+
+  // Routes
+// ===========================================================
+app.get('/', (req, res) => {
+    res.send('Welcome to the page!');
+    res.json(games)
+  });
+
+  // port test
+app.listen(PORT, () => {
+  console.log("API server Started on port !! !");
+});
