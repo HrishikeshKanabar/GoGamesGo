dab80ab3b4964fb2ddf9aa30bc2c2b0e087b0e28
Junya Qiao <junyaqiao@gmail.com>
2021-11-30 23:57:41 -0500

    add game_desc to db

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,17 +1,17 @@
-INSERT INTO games (game_title, publication_year, game_genre, console)
+INSERT INTO games (game_title, publication_year, game_genre,game_desc, console)
 VALUES
-('Final Fantasy VII', 1997, 'RPG', 'PlayStation 1'),
-('Red Dead Redemption', 2010, 'Action-Adventure', 'XBOX 360'),
-('The Witcher 3: Wild Hunt', 2015, 'RPG', 'PlayStation 4'),
-('Monster Hunter: World', 2018, 'RPG', 'PlayStation 4'),
-('Dark Souls', 2011, 'RPG', 'XBOX 360'),
-('Dark Souls 2', 2014, 'RPG', 'XBOX 360'),
-('Dark Souls 3', 2016, 'RPG', 'XBOX 360'),
-('Grand Theft Auto V', 2013, 'Action-Adventure', 'PlayStation 3'),
-('Animal Crossing: New Horizons', 2020, 'Simulation', 'Nintendo Switch'),
-('Pokemon Yellow', 1998, 'RPG', 'Game Boy'),
-('Suikoden 3', 2002, 'RPG', 'PlayStation 2'),
-('Call of Duty: BlackOps', 2010, 'FPS', 'PlayStation 3');
+('Final Fantasy VII', 1997, 'RPG', 'game_desc','PlayStation 1'),
+('Red Dead Redemption', 2010, 'Action-Adventure','game_desc', 'XBOX 360'),
+('The Witcher 3: Wild Hunt', 2015, 'RPG', 'game_desc','PlayStation 4'),
+('Monster Hunter: World', 2018, 'RPG', 'game_desc','PlayStation 4'),
+('Dark Souls', 2011, 'RPG', 'game_desc','XBOX 360'),
+('Dark Souls 2', 2014, 'RPG', 'game_desc','XBOX 360'),
+('Dark Souls 3', 2016, 'RPG','game_desc','XBOX 360'),
+('Grand Theft Auto V', 2013, 'Action-Adventure', 'game_desc','PlayStation 3'),
+('Animal Crossing: New Horizons', 2020, 'Simulation', 'game_desc','Nintendo Switch'),
+('Pokemon Yellow', 1998, 'RPG','game_desc', 'Game Boy'),
+('Suikoden 3', 2002, 'RPG', 'game_desc','PlayStation 2'),
+('Call of Duty: BlackOps', 2010, 'FPS','game_desc', 'PlayStation 3');
 
 INSERT INTO reviews (review_desc, rating, game_id)
 VALUES
