aeae6e90fb631f65518cf3fdb267232af2e51f9c
fairfax <lovelace.af@gmail.com>
2021-11-30 19:15:20 -0500

    updated ratings numbers

--- db/seeds.sql
+++ db/seeds.sql
@@ -15,15 +15,15 @@ VALUES
 
 INSERT INTO reviews (review_desc, rating, game_id)
 VALUES
-('One of the best JRPGS of all time!', 99, 1),
-("Who doesn't want to pretend to be a cowboy in the wild west?", 89, 2),
-('If you like expansive medieval fantasy stories, then this is the one for you!', 95, 3),
-('There is nothing more exciting than climbing a giant monster, and making armour from it.', 70, 4),
-('Renowned for its difficulty, Dark Souls is an immensely satisfying video game in a dark decaying world.', 93, 5),
-('Not as good as the first entry in the series. It was a disappointing addition to the series', 72, 6),
-('The final entry to the Dark Souls series. Rest in peace, Dark Souls. You were great.', 80, 7),
-('Rockstar makes great games, and GTA 5 is world of fun that you will keep you playing for hours', 83, 8),
-("A social simulation game that is fun to play if you enjoy a more relaxing game", 72, 9),
-("Everyone knows Pokemon! What is more fun that having Pikachu follow you around with the original Pokedex?", 88, 10),
-("Suikoden 3 is a flawed masterpiece, and it's Fairfax's favourite game of all time", 100, 11),
-("One of the original and best entries to the COD games!", 80, 12);
+('One of the best JRPGS of all time!', 5, 1),
+("Who doesn't want to pretend to be a cowboy in the wild west?", 4, 2),
+('If you like expansive medieval fantasy stories, then this is the one for you!', 4, 3),
+('There is nothing more exciting than climbing a giant monster, and making armour from it.', 3, 4),
+('Renowned for its difficulty, Dark Souls is an immensely satisfying video game in a dark decaying world.', 4, 5),
+('Not as good as the first entry in the series. It was a disappointing addition to the series', 3, 6),
+('The final entry to the Dark Souls series. Rest in peace, Dark Souls. You were great.', 4, 7),
+('Rockstar makes great games, and GTA 5 is world of fun that you will keep you playing for hours', 4, 8),
+("A social simulation game that is fun to play if you enjoy a more relaxing game", 3, 9),
+("Everyone knows Pokemon! What is more fun that having Pikachu follow you around with the original Pokedex?", 4, 10),
+("Suikoden 3 is a flawed masterpiece, and it's Fairfax's favourite game of all time", 5, 11),
+("One of the original and best entries to the COD games!", 5, 12);
