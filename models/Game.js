83888973f3c047b8bba1f12e8637f6ef4707b91d
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2021-12-04 10:35:04 -0500

    Re creating game

---
+++ models/Game.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize= require("../config/connection");
+// This model of Game capital
+class Game extends Model {}
+
+Game.init(
+    {
+      // define columns
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      game_title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      game_desc:
+      {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      publication_year: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      game_genre:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      console:{
+        type: DataTypes.STRING,
+        allowNull: true,
+      }
+    },
+    {
+      sequelize,
+      timestamps: true,
+      freezeTableName: true,
+      underscored: true,
+      modelName: "game",
+    }
+  );
+
+  
+  module.exports = Game;
